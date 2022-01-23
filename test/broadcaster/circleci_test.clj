2c390457fa3838d3f0afb29239d2a84b06b7f5f9
Rune Nielsen <runenielsen@runbox.com>
2022-01-23 10:45:49 +0100

    adds test data for circleci webhooks (#6)

--- test/broadcaster/circleci_test.clj
+++ test/broadcaster/circleci_test.clj
@@ -1,21 +1,14 @@
 (ns broadcaster.circleci-test
   (:require [broadcaster.circleci :as sut]
+            [broadcaster.test-data :as test-data]
             [clojure.test :refer [deftest is]]))
 
 (deftest invalid-signature-request
-  (let [request {:headers {"content-type" "application/json"
-                           "user-agent" "CircleCI-Webhook/1.0"
-                           "circleci-event-type" "workflow-completed"
-                           "circleci-signature" "not-a-valid-signature"}
-                 :body "foo"}
-        result (sut/circleci-request request "secret")]
+  (let [request test-data/invalid-signature-request
+        result (sut/circleci-request request test-data/secret)]
     (is (false? result))))
 
 (deftest circleci-request
-  (let [request {:headers {"content-type" "application/json"
-                           "user-agent" "CircleCI-Webhook/1.0"
-                           "circleci-event-type" "workflow-completed"
-                           "circleci-signature" "773ba44693c7553d6ee20f61ea5d2757a9a4f4a44d2841ae4e95b52e4cd62db4"}
-                 :body "foo"}
-        result (sut/circleci-request request "secret")]
+  (let [request test-data/job-completed-request
+        result (sut/circleci-request request test-data/secret)]
     (is (true? result))))
