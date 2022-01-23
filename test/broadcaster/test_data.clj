b90c0826dd2c177ad8ae152fb0015958528e6e9e
Rune Nielsen <runenielsen@runbox.com>
2022-01-23 10:37:07 +0100

    adds test data for circleci webhooks

---
+++ test/broadcaster/test_data.clj
@@ -0,0 +1,28 @@
+(ns broadcaster.test-data)
+
+(def ^:private test-data-path "test/broadcaster/test_data/")
+(def ^:private workflow-completed-body-json (slurp (str test-data-path "workflow-completed.json")))
+(def ^:private job-completed-body-json (slurp (str test-data-path "job-completed.json")))
+
+(def secret "my-secret123")
+
+(def job-completed-request
+  {:headers {"content-type" "application/json"
+             "user-agent" "CircleCI/Webhook/1.0"
+             "circleci-event-type" "job-completed"
+             "circleci-signature" "5a187556ae2d6d0be9212883287f59cb3160deff934bd94a75ebe64a1c7a56c4"}
+   :body job-completed-body-json})
+
+(def workflow-completed-request
+  {:headers {"content-type" "application/json"
+             "user-agent" "CircleCI/Webhook/1.0"
+             "circleci-event-type" "workflow-completed"
+             "circleci-signature" "70dc0980c4f5e7d903072701576ad1af508701a092757e2a8869650ba23ee834"}
+   :body workflow-completed-body-json})
+
+(def invalid-signature-request
+  {:headers {"content-type" "application/json"
+             "user-agent" "CircleCI/Webhook/1.0"
+             "circleci-event-type" "workflow-completed"
+             "circleci-signature" "this-signature-is-invalid"}
+   :body workflow-completed-body-json})
